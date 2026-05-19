c4a0632691befb69dd121ec12648aeaf524fbfb1
zoom26042604 <nathan.ferre@ynov.com>
2026-05-19 15:10:39 +0000

    fix: restore JWT session on reload, fix page backgrounds

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar';
 import { BottomNavComponent } from './components/bottom-nav/bottom-nav';
+import { AuthService } from './core/services/auth/auth-service';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,15 @@ import { BottomNavComponent } from './components/bottom-nav/bottom-nav';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
   protected title = 'asteria-front';
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.authService.getUserInfo().subscribe({
+      next: (user) => this.authService.setCurrentUser(user),
+      error: () => {},
+    });
+  }
 }
